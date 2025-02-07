91aa5ad1bd183a6e25d833dbc0bbd1259b2a77a1
Tofikoabdu1 <165275773+Tofikoabdu1@users.noreply.github.com>
2025-02-07 11:09:33 +0300

    Update Anton and Danik.cpp

--- Anton and Danik.cpp
+++ Anton and Danik.cpp
@@ -1,1 +1,19 @@
-
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    string a;
+    cin>>a;
+    int anton=0;
+    int danik=0;
+    for(char ch : a){
+        if(ch=='A')anton++;
+        else if(ch=='D')danik++;
+    }
+    if (anton>danik){cout << "Anton" <<endl;}
+    else if(anton<danik){cout << "Danik" <<endl;}
+    else if (anton==danik){cout << "Friendship" <<endl;}
+    return 0;
+}
