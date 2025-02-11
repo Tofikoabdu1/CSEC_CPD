faf84b1620c989980c4adfea4695c2acce197ca2
Tofikoabdu1 <165275773+Tofikoabdu1@users.noreply.github.com>
2025-02-11 09:17:09 +0300

    Create Boy or Girl.cpp

---
+++ Boy or Girl.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <set>
+#include <string>
+using namespace std;
+int main() {
+    string username;
+    cin >> username; 
+    set<char> distinctChars; 
+    for (char c : username) {
+        distinctChars.insert(c);
+    }
+    if (distinctChars.size() % 2 == 0) {
+        cout << "CHAT WITH HER!" << endl; 
+    } else {
+        cout << "IGNORE HIM!" << endl; 
+    }
+    return 0;
+}
