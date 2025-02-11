e25d5f70c9113f3938ad0768522d6978ab9fb2c0
Tofikoabdu1 <165275773+Tofikoabdu1@users.noreply.github.com>
2025-02-11 11:48:10 +0300

    Create buy a shovel.cpp

---
+++ buy a shovel.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int k, r;
+    cin >> k >> r; 
+    for (int n = 1; n <= 10; ++n) { 
+        int totalCost = n * k; 
+        if (totalCost % 10 == 0 || totalCost % 10 == r) {
+            cout << n << endl; 
+            return 0;
+        }
+    }
+    return 0; 
+}
